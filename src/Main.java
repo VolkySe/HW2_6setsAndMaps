43525622a29bbf75498d0bbf53bf9974d8bfcc0f
VolkySe <sergej_lykov@mail.ru>
2025-03-11 15:09:49 +0300

    Task2 corrected. HW 2_6 Stes And Maps done

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.TreeSet;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(1, 12, 2, 3, 34, 4, 5, 5, 6, 7));
         List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
         System.out.println(nums);
@@ -33,7 +34,13 @@ public class Main {
     }
 
     private static void task2(List<Integer> nums) {
-        System.out.println(new HashSet<>(nums));
+        TreeSet<Integer> numsTreeSet= new TreeSet<>(nums);
+        for (Integer i : numsTreeSet) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 
     private static void task3(List<String> strings) {
