01d946cc33800056aa5194906f31c5460d37ff55
VolkySe <sergej_lykov@mail.ru>
2025-03-11 14:27:01 +0300

    Example Lists initializied.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+
+        System.out.println(nums);
+        System.out.println(strings);
 
     }
 }
